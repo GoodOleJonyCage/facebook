5272c9a510801bbba8f0a8de81b0b4b1313d56a2
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2021-09-27 12:15:15 -0400

    Project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Button variant="contained">Default</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
